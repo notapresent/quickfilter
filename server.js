68629ff75f3f08d195c4ad1bf8788e54f97b6d5f
Glitch (quickfilter) <none>
2018-11-22 17:11:09 +0000

    👩‍👩‍👦🔮 Implement request proxy

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 
 // init project
 const express = require('express');
+const proxy = require('express-http-proxy');
+const url = require('url');
+
+
 const app = express();
 
 // we've started you off with Express, 
@@ -15,6 +19,21 @@ app.use(express.static('public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+function selectProxyHost(container) {
+    const parsed = url.parse(container.url.substr(1));
+    return parsed.protocol + '//' + parsed.host;
+}
+
+function resolvePath(req) {
+    const parsed = url.parse(req.url.substr(1));
+    return parsed.path + (parsed.hash || '');    
+}
+
+app.use('/proxy', proxy(selectProxyHost, {
+    memoizeHost: false,
+    proxyReqPathResolver: resolvePath
+}));
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
